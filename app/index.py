c2d6116efc55b979970fbc19a76b5c8bff55decb
smeghead <smeghead7@gmail.com>
2009-10-29 20:39:55 +0900

    sample complete.

--- app/index.py
+++ app/index.py
@@ -1,19 +1,53 @@
+import os
+import cgi
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+from google.appengine.ext import db
+
+class Greeting(db.Model):
+    author = db.UserProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
 
+        greeting_query = Greeting.all().order('-date')
+        greetings = greeting_query.fetch(10)
+        
         if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello, ' + user.nickname())
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
         else:
-            self.redirect(users.create_login_url(self.request.uri))
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        params = {
+            'greetings': greetings,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, params))
+
+class Guestbook(webapp.RequestHandler):
+    def post(self):
+        greeting = Greeting()
+
+        if users.get_current_user():
+            greeting.author = users.get_current_user()
+
+        greeting.content = self.request.get('content')
+        greeting.put()
+        self.redirect('/')
 
 application = webapp.WSGIApplication(
-    [('/', MainPage)],
+    [('/', MainPage),
+     ('/sign', Guestbook)],
     debug=True)
 
 def main():
