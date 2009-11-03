60368d5f864abe6a32134eced275ee35ef838bf2
smeghead <smeghead7@gmail.com>
2009-11-03 19:48:30 +0900

    required field.

--- app/index.py
+++ app/index.py
@@ -10,15 +10,13 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 
 class Ticket(db.Model):
-    id = db.IntegerProperty(long)
+    id = db.IntegerProperty(long, required=True)
     date = db.DateTimeProperty(auto_now_add=True)
     messages = db.ListProperty(db.Key)
     author = db.UserProperty()
 
 class Message(db.Model):
-    author = db.UserProperty(required=True)
     field1 = db.StringProperty(required=True)
-    field2 = db.StringProperty(required=True)
     field3 = db.StringProperty(required=True)
     field4 = db.ListProperty(unicode)
     field5 = db.StringProperty()
@@ -38,20 +36,21 @@ class MainPage(webapp.RequestHandler):
         for ticket in tickets_query:
             if len(ticket.messages) > 0:
                 message = Message.get(ticket.messages[len(ticket.messages) - 1])
-            tickets.append({
-                'ticket': ticket,
-                'message': message
-            })
+                tickets.append({
+                    'ticket': ticket,
+                    'message': message
+                })
 
         if user:
             url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
+            url_linktext = 'ログアウト'
         else:
             url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
+            url_linktext = 'ログイン'
 
         params = {
             'tickets': tickets,
+            'user': user,
             'url': url,
             'url_linktext': url_linktext,
         }
@@ -59,44 +58,20 @@ class MainPage(webapp.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, params))
 
-class Guestbook(webapp.RequestHandler):
-    def post(self):
-        greeting = Greeting()
-
-        if users.get_current_user():
-            greeting.author = users.get_current_user()
-
-        greeting.content = self.request.get('content')
-        greeting.put()
-        self.redirect('/')
-
-class Register(webapp.RequestHandler):
+class View(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
 
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        path = {
-            'script_name': os.environ['SCRIPT_NAME']
-        }
         params = {
-            'path': path,
-            'url': url,
-            'url_linktext': url_linktext,
+            'user': user
         }
 
-        path = os.path.join(os.path.dirname(__file__), 'register.html')
+        path = os.path.join(os.path.dirname(__file__), 'view.html')
         self.response.out.write(template.render(path, params))
     
     def post(self):
         message_posted = {
             'field1': self.request.get('Message.field1'),
-            'field2': self.request.get('Message.field2'),
             'field3': self.request.get('Message.field3'),
             'field4': self.request.get_all('Message.field4'),
             'field5': self.request.get('Message.field5'),
@@ -106,20 +81,58 @@ class Register(webapp.RequestHandler):
         }
         try:
             user = users.get_current_user()
-            ticket = Ticket()
+#             ticket = Ticket(id=max_id+1)
+#             if user:
+#                 ticket.author = user
+#             ticket.put()
+#             message = Message(parent=ticket, **message_posted)
+#             if user:
+#                 message.author = user
+#             message.put()
+#             ticket.messages.append(message.key())
+#             ticket.put()
+#             self.redirect('/')
+        except Exception, mes:
+            path = os.path.join(os.path.dirname(__file__), 'view.html')
+            self.response.out.write(template.render(path, {
+                'errormessage': mes,
+                'message': message_posted
+            }))
+
+
+class Register(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+
+        params = {
+            'user': user
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'register.html')
+        self.response.out.write(template.render(path, params))
+    
+    def post(self):
+        message_posted = {
+            'field1': self.request.get('Message.field1'),
+            'field3': self.request.get('Message.field3'),
+            'field4': self.request.get_all('Message.field4'),
+            'field5': self.request.get('Message.field5'),
+            'field6': self.request.get('Message.field6'),
+            'field7': self.request.get('Message.field7'),
+            'field8': self.request.get('Message.field8'),
+        }
+        try:
+            lastticket = Ticket.all().order('-id').fetch(1)
+            if len(lastticket) == 0:
+                next_id = 1
+            else:
+                next_id = lastticket[0].id + 1
+            user = users.get_current_user()
+            ticket = Ticket(id=next_id)
             if user:
                 ticket.author = user
             ticket.put()
             message = Message(parent=ticket, **message_posted)
-#             message.field1 = message_posted.field1
-#             message.field2 = message_posted.field2
-#             message.field3 = message_posted.field3
-#             message.field4 = message_posted.field4
-#             #message.field4 = map(lambda x: unicode(x, 'utf-8'), fields)
-#             message.field5 = message_posted.field5
-#             message.field6 = message_posted.field6
-#             message.field7 = message_posted.field7
-#             message.field8 = message_posted.field8
             if user:
                 message.author = user
             message.put()
@@ -133,11 +146,10 @@ class Register(webapp.RequestHandler):
                 'message': message_posted
             }))
 
-
 application = webapp.WSGIApplication(
     [('/', MainPage),
      ('/register', Register),
-     ('/sign', Guestbook)],
+     ('/view', View)],
     debug=True)
 
 def main():
